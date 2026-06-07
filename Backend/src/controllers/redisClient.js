395ba76a80d54a48ffb1734d8c76d8c8019c82d8
Md-musavvir <musavvirahmed490@gmail.com>
2026-06-07 16:35:38 +0530

    fix: add Redis client setup and update package dependencies

---
+++ Backend/src/controllers/redisClient.js
@@ -0,0 +1,9 @@
+import { createClient } from "redis";
+
+const redisClient = createClient();
+redisClient.on("error", (err) => {
+  console.log("Redis Error:", err);
+});
+await redisClient.connect();
+console.log("redis connected");
+export default redisClient;
