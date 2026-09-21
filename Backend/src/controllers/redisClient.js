1f465f11ea74a8d273efb77d2d52cc4ce6ffa7fa
Md-musavvir <musavvirahmed490@gmail.com>
2026-09-21 19:32:04 +0530

    fix: update Redis client implementation to use ioredis

--- Backend/src/controllers/redisClient.js
+++ Backend/src/controllers/redisClient.js
@@ -1,11 +1,13 @@
-import { createClient } from "redis";
+import Redis from "ioredis";
 
-const redisClient = createClient({
-  url: process.env.REDIS_URL,
-});
+const redisClient = new Redis(process.env.REDIS_URL);
+
 redisClient.on("error", (err) => {
   console.log("Redis Error:", err);
 });
-await redisClient.connect();
-console.log("redis connected");
+
+redisClient.on("connect", () => {
+  console.log("redis connected");
+});
+
 export default redisClient;
