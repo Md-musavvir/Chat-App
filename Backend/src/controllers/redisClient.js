497205ee8e57ab2274a2b4420e81c2179ee292bf
Md-musavvir <musavvirahmed490@gmail.com>
2026-09-21 19:28:49 +0530

    fix: remove socket options from redis client configuration

--- Backend/src/controllers/redisClient.js
+++ Backend/src/controllers/redisClient.js
@@ -2,10 +2,6 @@ import { createClient } from "redis";
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
-  socket: {
-    tls: true,
-    rejectUnauthorized: false,
-  },
 });
 redisClient.on("error", (err) => {
   console.log("Redis Error:", err);
